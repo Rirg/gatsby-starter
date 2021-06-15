dd3a211c38bc0af4aa25e4031825a2e2615b0fce
Rick <rick@weareshellshock.com>
2021-06-15 13:11:40 -0500

    remove import

--- src/templates/post/post.template.jsx
+++ src/templates/post/post.template.jsx
@@ -1,16 +1,15 @@
 import React from "react"
 import Layout from "../../components/layout"
-import Img from "gatsby-image"
 
-const Post = ({pageContext: {data}}) => {
-  const {seo, title, content, featuredImage} = data
+const Post = ({ pageContext: { data } }) => {
+  const { seo, title, content, featuredImage } = data
   return (
     <Layout seo={seo}>
       {title}
-      <div dangerouslySetInnerHTML={{__html: content}} />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
       {/*<Img fluid={featuredImage.imageFile.childImageSharp.fluid} />*/}
     </Layout>
   )
 }
 
-export default Post;
+export default Post
