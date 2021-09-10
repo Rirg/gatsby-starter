961f81811bc01f32b9eac6f5253b0ef7f7e0673c
Ricardo <41655800+Rirg94@users.noreply.github.com>
2021-09-10 14:52:24 -0500

    Update layouts.js

--- src/pages/layouts.js
+++ src/pages/layouts.js
@@ -5,10 +5,15 @@ import { LayoutTitle } from "../styles/app.styles"
 
 const Layouts = () => {
   return (
-    <Layout seo={{ title: "Layouts - ONLY FOR DEV" }}>
-      <LayoutTitle>MiscContent</LayoutTitle>
-      <MiscContent content={`<p>Some content Here</p>`} />
-    </Layout>
+    <>
+      <Helmet>
+        <meta name={`robots`} content={`noindex, nofollow`} />
+      </Helmet>
+      <Layout seo={{ title: "Layouts - ONLY FOR DEV" }}>
+        <LayoutTitle>MiscContent</LayoutTitle>
+        <MiscContent content={`<p>Some content Here</p>`} />
+      </Layout>
+    </>
   )
 }
 
